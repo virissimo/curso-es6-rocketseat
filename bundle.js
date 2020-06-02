dd6b664591217948fe64ac16e102a0dc356245f4
Francisco Virissimo <virissimo.francisco@gmail.com>
2020-06-02 12:15:36 -0300

    Criação e configuração inicial do projeto.

---
+++ bundle.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+alert('teste');
+
+var Teste = function Teste() {
+  _classCallCheck(this, Teste);
+};
